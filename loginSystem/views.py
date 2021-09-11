464f8e931f544450f8eafd7e802108de9d906398
asstronut <akbar.10119247@mahasiswa.unikom.ac.id>
2021-09-11 16:32:23 +0700

    Sign Up system: CHECK

---
+++ loginSystem/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render, redirect, reverse
+from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from .forms import UserRegisterForm
+
+def index(request):
+    context = {
+        'title': 'User System App'
+    }
+    return render(request, 'loginSystem/index.html', context)
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, message='Registration ')
+            return redirect(reverse('loginSystem:index'))
+    else:
+        form = UserRegisterForm()
+    context = {
+        'title': 'Registration Page',
+        'form': form
+    }
+    return render(request, 'loginSystem/register.html', context)
+
+
+def login(request):
+    return render(request, 'loginSystem/login.html')
